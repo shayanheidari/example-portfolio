b8d8adb6f0d73f9c72e4d9a8582e40273f87c9c0
shayan <shaian.heidari@gmail.com>
2025-05-28 02:47:40 +0330

    init

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -14,11 +14,11 @@ const Background = () => {
           </h1>
         </div>
         <div className="w-full md:w-[65vw] md:h-[70vh] md:mt-30 md:grid md:grid-cols-2 md:grid-rows-2 md:gap-5 md:justify-items-stretch  md:items-center overflow-x-scroll md:overflow-auto scroll whitespace-nowrap scroll-smooth px-4 snap-x md:snap-none no-scrollbar mt-20">
-          <div className="md:m-0 snap-center md:w-auto rounded-3xl whitespace-normal card_ inline-block m-2  no-scrollbar">
+          <div className="ml-15 md:m-0 snap-center w-full md:w-auto rounded-3xl whitespace-normal card_ inline-block m-2  no-scrollbar">
             <h3 className="font-semibold text-2xl text-accent pb-3">
               Education
             </h3>
-            <p className="text-pretty text-text h-55 md:h-auto">
+            <p className="text-pretty text-text  md:h-auto h-[calc(100%-70px)] overflow-y-scroll no-scrollbar">
               <span className="block italic text-[1rem] text-accent">
                 2020 – 2024
               </span>
@@ -40,11 +40,11 @@ const Background = () => {
               , Science and Research Branch, Azad University, Iran
             </p>
           </div>
-          <div className="md:m-0 snap-center md:w-auto rounded-3xl whitespace-normal card_ inline-block m-2  no-scrollbar">
+          <div className="md:m-0 snap-center w-full md:w-auto rounded-3xl whitespace-normal card_ inline-block m-2  no-scrollbar">
             <h3 className="font-semibold text-2xl text-accent pb-3">
               Postgraduate Training
             </h3>
-            <p className="text-pretty text-text  h-45 md:h-auto">
+            <p className="text-pretty text-text  md:h-auto h-[calc(100%-70px)] overflow-y-scroll no-scrollbar">
               <span className="block italic text-[1rem] text-accent">
                 2024 – 2025
               </span>
@@ -54,11 +54,11 @@ const Background = () => {
               strategies for biosecurity computer vision applications.
             </p>
           </div>
-          <div className="md:m-0  snap-center rounded-3xl whitespace-normal card_ inline-block m-2 md:col-span-2  no-scrollbar md:w-[70%] md:!mx-auto">
+          <div className="mr-15 md:m-0  snap-center w-full rounded-3xl whitespace-normal card_ inline-block m-2 md:col-span-2  no-scrollbar md:w-[70%] md:!mx-auto">
             <h3 className="font-semibold text-2xl text-accent pb-3">
               Current Positions
             </h3>
-            <p className="text-pretty text-text h-66 md:h-auto w-full">
+            <p className="text-pretty text-text  md:h-auto h-[calc(100%-70px)] overflow-y-scroll no-scrollbar">
               <span className="block italic text-[1rem] text-accent">
                 2025 – Present
               </span>
