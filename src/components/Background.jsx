1e6a6eb771f030b51fa8eec8953686705f48a624
shayan <shaian.heidari@gmail.com>
2025-05-28 02:27:55 +0330

    init

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router";
 
 const Background = () => {
   return (
@@ -19,21 +18,16 @@ const Background = () => {
               Education
             </h3>
             <p className="text-pretty text-text h-55 md:h-auto">
-              <h4 className="italic text-[1rem] text-accent">2020 – 2024</h4>
+              <span className="block italic text-[1rem] text-accent">
+                2020 – 2024
+              </span>
               <span className="font-normal">Ph.D. in Computer Science</span>,
               The University of Auckland, New Zealand
               <br />
-              Thesis:{" "}
-              <Link
-                to="/example-portfolio/works/thesis/"
-                className="italic underline hover:text-accent cursor-pointer"
-              >
-                Quantum Annealing in Computer Vision
-              </Link>
-              <br />
-              <h4 className="italic mt-2 md:mt-3 text-[1rem] text-accent">
+              Thesis: <br />
+              <span className="block italic mt-2 md:mt-3 text-[1rem] text-accent">
                 2014 – 2016
-              </h4>
+              </span>
               <span className="font-normal">M.Sc. in Software Engineering</span>
               , Science and Research Branch, Azad University, Iran
             </p>
@@ -43,7 +37,9 @@ const Background = () => {
               Postgraduate Training
             </h3>
             <p className="text-pretty text-text  h-45 md:h-auto">
-              <h4 className="italic text-[1rem] text-accent">2024 – 2025</h4>
+              <span className="block italic text-[1rem] text-accent">
+                2024 – 2025
+              </span>
               <span className="font-normal">Postdoctoral Fellow</span> in
               Computer Science, The University of Auckland, New Zealand <br />{" "}
               <div className="h-2" /> Research Focus: AI model improvement
@@ -52,17 +48,19 @@ const Background = () => {
           </div>
           <div className="mr-15 md:m-0  snap-center w-4/5 rounded-3xl whitespace-normal card_ inline-block m-2 md:col-span-2  no-scrollbar md:w-[70%] md:!mx-auto">
             <h3 className="font-semibold text-2xl text-accent pb-3">
-              Current Position
+              Current Positions
             </h3>
             <p className="text-pretty text-text h-66 md:h-auto w-full">
-              <h4 className="italic text-[1rem] text-accent">2025 – Present</h4>
+              <span className="block italic text-[1rem] text-accent">
+                2025 – Present
+              </span>
               <span className="font-normal">Research Fellow</span>, Institute of
               Marine Science, The University of Auckland, New Zealand
               <br /> <div className="h-2" /> Research Focus: Advancing computer
               vision and photogrammetry for benthic ecosystems
-              <h4 className="italic mt-2 text-[1rem] text-accent">
+              <span className="block italic mt-2 text-[1rem] text-accent">
                 2020 – Present
-              </h4>
+              </span>
               <span className="font-normal">Research Collaborator</span>,
               Intelligent Vision System Lab (
               <a
