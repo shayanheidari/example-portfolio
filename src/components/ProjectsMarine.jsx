d55a4a1e342c228be4b4542075da2052a0309a94
shayan <shaian.heidari@gmail.com>
2025-06-05 02:29:49 +0330

    init

--- src/components/ProjectsMarine.jsx
+++ src/components/ProjectsMarine.jsx
@@ -5,7 +5,6 @@ import project2 from "../assets/works/marine/2/Figure6.jpg";
 import project3 from "../assets/project3.png";
 import project4 from "../assets/project4.png";
 import project5 from "../assets/project5.png";
-import project6 from "../assets/project6.png";
 import {
   PiNumberCircleOneFill,
   PiNumberCircleOneLight,
--- src/components/ProjectsSide.jsx
+++ src/components/ProjectsSide.jsx
@@ -5,7 +5,6 @@ import project2 from "../assets/project1.png";
 import project3 from "../assets/project3.png";
 import project4 from "../assets/project4.png";
 import project5 from "../assets/project5.png";
-import project6 from "../assets/project6.png";
 import {
   PiNumberCircleOneFill,
   PiNumberCircleOneLight,
